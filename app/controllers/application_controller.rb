7d534f159ab9a9c63926ebaba2ade304f6704e93
Annemarie <a.lucernoni0@gmail.com>
2022-07-06 06:16:13 -0700

    commit tree e116909786da2e662a0037b1d197d165477eb716

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+    def render_invalid_response(e)
+        render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def render_not_found(e)
+        render json: {errors: "#{e.model} not found"}, status: :not_found
+    end
+
 end
